c41b81cde163aa77c001bd278b3bb422394ed3f9
bitnaleeeee <>
2022-11-17 22:25:15 +0900

    1차PR

--- src/views/Slider.js
+++ src/views/Slider.js
@@ -8,7 +8,7 @@ const Slider = () => {
       <div className="sliderWrap">
         <div className="navHeader">DECEMBER</div>
         <div className="user">
-          <Link to="./login"> 투자계좌</Link>
+          <Link to="./"> 투자계좌</Link>
         </div>
         <div className="account">사용자</div>
         <div className="logout">로그아웃</div>
