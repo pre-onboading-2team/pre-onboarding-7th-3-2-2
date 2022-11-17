09000a56c152e542bfa01d6970fda82a18e6eea1
bitnaleeeee <>
2022-11-17 14:57:27 +0900

    rows추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import DataTable from '../../views/DataTable';
 import Slider from '../../views/Slider';
-import axios from 'axios';
 import './Main.scss';
 
 const Main = () => {
   const [data, setData] = useState([]);
+  const [user, setUser] = useState([]);
   const token = localStorage.getItem('token');
 
-  const header = {
-    headers: {
-      Authorization: 'Bearer ' + token,
-    },
-  };
   useEffect(() => {
-    axios
-      .get('http://localhost:4000/accounts', header)
-      .then(({ data }) => setData(data));
+    fetch('http://localhost:4000/users', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then(response => response.json())
+      .then(result => {
+        setUser(result);
+      });
   }, []);
-  console.log(data);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/accounts', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then(response => response.json())
+      .then(result => {
+        setData(result);
+      });
+  }, []);
 
   return (
     <>
       <div className="mainWrap">
         <Slider />
-        <DataTable />
+        <DataTable data={data} user={user} />
       </div>
       <div className="siderBar" />
       <div className="contentWrap" />
--- src/views/DataTable.js
+++ src/views/DataTable.js
@@ -2,65 +2,40 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: '증권사', width: 70 },
-  { field: 'firstName', headerName: '계좌번호', width: 80 },
-  { field: 'lastName', headerName: '고객명', width: 70 },
-  {
-    field: 'age',
-    headerName: '운용상태',
-    type: 'number',
-    width: 100,
-  },
-  {
-    field: 'principal',
-    headerName: '계약원금',
-    width: 150,
-    valueGetter: params =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  {
-    field: 'fullName',
-    headerName: '총자산',
-    width: 100,
-    valueGetter: params =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  {
-    field: 'profitOrLoss',
-    headerName: '평가손익',
-    width: 100,
-    valueGetter: params =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  {
-    field: 'profitRate',
-    headerName: '수익률',
-    width: 100,
-    valueGetter: params =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  {
-    field: 'productName',
-    headerName: '상품명',
-    width: 100,
-    valueGetter: params =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
+  { field: 'userName', headerName: '고객명', width: 70 },
+  { field: 'brokerName', headerName: '증권사', width: 80 },
+  { field: 'number', headerName: '계좌번호', width: 80 },
+  { field: 'status', headerName: '운용상태', width: 70 },
+  { field: 'name', headerName: '계좌명', width: 100 },
+  { field: 'assets', headerName: '평가금액', width: 150 },
+  { field: 'payments', headerName: '입금금액', width: 100 },
+  { field: 'is_active', headerName: '활성화', width: 100 },
+  { field: 'created_at', headerName: '계좌개설일', width: 100 },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const rowsData = [];
+rowsData.forEach(data => {
+  rowsData.push({
+    id: data.id,
+    userName: data.userName,
+    brokerName: data.broker_id,
+    number: data.number,
+    status: data.status,
+    name: data.name,
+    assets: data.assets,
+    payments: data.payments,
+    is_active: data.is_active,
+    created_at: data.created_at,
+  });
+});
 
-export default function DataTable() {
+const rows = rowsData;
+
+export default function DataTable(props) {
+  const { data, user } = props;
+  console.log(data);
+  console.log(user);
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
